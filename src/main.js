2c37bef55dffacaddfd72cf2bdd26490b25820bc
junyoung.jung <junyoung.jung@hyundai-autoever.com>
2023-05-10 11:51:03 +0900

    [2023.05.10] vue-router, v-calendar 제거

--- src/main.js
+++ src/main.js
@@ -1,31 +1,12 @@
 import { createApp } from 'vue'
-import { createWebHashHistory, createRouter } from 'vue-router'
-import VCalendar from 'v-calendar';
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-import 'v-calendar/style.css';
 import 'animate.css';
 
 import App from './App'
-
-const routes = [
-  { path: '/', component: App },
-]
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { x: 0, y: 0}
-    }
-  }
-})
 
 const vuetify = createVuetify({
   components,
@@ -33,7 +14,5 @@ const vuetify = createVuetify({
 })
 
 const app = createApp(App)
-app.use(router)
 app.use(vuetify)
-app.use(VCalendar, {})
 app.mount('#app')
